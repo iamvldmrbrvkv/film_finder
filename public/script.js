ca6b60848b602b0461ab5bc64c1688bf5070fd64
Vladimir Borovikov <91381325+iamvldmrbrvkv@users.noreply.github.com>
2023-08-16 14:50:35 +0300

    fixed script.js

--- public/script.js
+++ public/script.js
@@ -3,21 +3,21 @@ const tmdbBaseUrl = 'https://api.themoviedb.org/3';
 const playBtn = document.getElementById('playBtn');
 
 const getGenres = async () => {
-const genreRequestEndpoint = '/genre/movie/list';
-const requestParams = `?api_key=${tmdbKey}`;
-const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
-try {
-  const response = await fetch(urlToFetch);
-  if (response.ok) {
-    const jsonResponse = await response.json();
-    // console.log(jsonResponse);
-    const genres = jsonResponse.genres;
-    // console.log(genres);
-    return genres;
+  const genreRequestEndpoint = '/genre/movie/list';
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      // console.log(jsonResponse);
+      const genres = jsonResponse.genres;
+      // console.log(genres);
+      return genres;
+    }
+  } catch (error) {
+    console.log(error);
   }
-} catch (error) {
-  console.log(error);
-}
 };
 
 const getMovies = async () => {
